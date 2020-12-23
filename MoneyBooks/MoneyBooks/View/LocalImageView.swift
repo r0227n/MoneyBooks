858dace53a8470181377128d8a05db178ef132bf
r0227n <b1803518@planet.kanazawa-it.ac.jp>
2020-12-23 23:19:32 +0900

    ローカル画像を読み込み、表示するサンプルプログラムの追加(要編集)

---
+++ MoneyBooks/MoneyBooks/View/LocalImageView.swift
@@ -0,0 +1,77 @@
+//
+//  LocalImageView.swift
+//  
+//
+//  Created by RyoNishimura on 2020/12/23.
+//
+
+import SwiftUI
+
+struct LocalImageView: View {
+    @State private var image: Image = Image("sea")
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
+    var body: some View {
+        Button(action: {
+            print("Tapped")
+            self.showingImagePicker = true
+        }) {
+            image
+                .resizable()
+                .renderingMode(.original)
+                .frame(width: 100, height: 100)
+            
+            Text("Tap Me!!")
+        }.sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
+}
+
+struct LocalImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocalImageView()
+    }
+}
+
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage?
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+
+    }
+}
